d1a03859f4cef9b6b49ad594d6d7357eb0b043f3
Gustavo Alves <gu.gu80@outlook.com>
2025-11-10 19:13:12 -0300

    Limpeza no código

--- 450.py
+++ 450.py
@@ -7,7 +7,7 @@ class TreeNode:
 
 class Solution:
     def _find_min(self, node: TreeNode) -> TreeNode:
-        # Função auxiliar para encontrar o menor valor na subárvore (o mais à esquerda)
+        # Função auxiliar para encontrar o menor valor na subárvore
         current = node
         while current.left is not None:
             current = current.left
@@ -26,22 +26,22 @@ class Solution:
             # Se a chave é maior, vá para a subárvore direita
             root.right = self.deleteNode(root.right, key)
         
-        # 2. O nó a ser deletado FOI ENCONTRADO (key == root.val)
+        # 2. O nó a ser deletado FOI ENCONTRADO
         else:
             # Caso A: Nó é folha ou tem apenas 1 filho
             if not root.left:
-                return root.right # Substitui pelo filho direito (pode ser None)
+                return root.right # Substitui pelo filho direito
             elif not root.right:
                 return root.left  # Substitui pelo filho esquerdo
 
-            # Caso B: O nó tem DOIS FILHOS (O caso mais difícil)
-            # a) Encontra o sucessor in-order (o nó de menor valor na subárvore direita)
+            # Caso B: O nó tem DOIS FILHOS
+            # a) Encontra o sucessor in-order
             successor = self._find_min(root.right)
             
-            # b) Copia o valor do sucessor para o nó atual (o que vai ser "deletado")
+            # b) Copia o valor do sucessor para o nó atual
             root.val = successor.val
             
-            # c) Deleta o sucessor (que agora é um nó com no máximo um filho, facilitando a deleção recursiva)
+            # c) Deleta o sucessor
             root.right = self.deleteNode(root.right, successor.val)
 
         return root
